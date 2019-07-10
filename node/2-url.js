da76c6173eaf96dd4d35e216723d364d15cb6548
Huanhua Su <karawakara@gmail.com>
2019-07-09 23:20:37 -0700

    Doing node Ex 2

--- node/2-url.js
+++ node/2-url.js
@@ -4,6 +4,46 @@ var http = require('http'); // do not change this line
 var url = require('url'); // do not change this line
 var querystring = require('querystring'); // do not change this line
 
+
+var port = Number(process.env.PORT);
+var hostname = 'localhost';
+
+
+var server = http.createServer(function (request, response) {
+    var thisURL = url.parse(request.url, true);
+    response.statusCode = 200;
+    response.setHeader('content-type', 'text/plain');
+
+    if (thisURL.pathname === '/') {
+        response.write('you have accessed the root');
+    }
+    else if (thisURL.pathname.includes('/test/')) {
+        response.write('you have accessed "' + thisURL.pathname.replace('/test/', '') + '" within test');
+    }
+    else if (request.url.includes('/attributes?')) {
+        response.setHeader('content-type', 'text/html');
+        var qy = thisURL.query;
+
+        var str = '<!DOCTYPE html>' +
+            '<html>' +
+            '<body>' +
+            '<table border="1">';
+        for (var key in qy) {
+            str += '<tr><td>' + key + '</td><td>' + qy[key] + '</td></tr>';
+        }
+        str += '</table></body></html>';
+        response.write(str);
+    }
+
+    response.end();
+
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:${port}/');
+});
+
+
 // http://localhost:8080/ should return 'you have accessed the root' in plain text
 
 // http://localhost:8080/test/hello should return 'you have accessed "hello" within test' in plain text
