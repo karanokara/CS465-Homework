858597b4f49f488fb9934d03e51fc0f1e3d2d9cf
Huanhua Su <karawakara@gmail.com>
2019-07-09 22:38:45 -0700

    Finish node Ex 1

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -2,4 +2,20 @@
 
 var http = require('http'); // do not change this line
 
+var port = Number(process.env.PORT);
+var hostname = 'localhost';
+
+var server = http.createServer(function (request, response) {
+    console.log(request);
+    response.statusCode = 200;
+    response.setHeader('content-type', 'text/html');
+    response.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+    response.end('<!-- < E N D > -->');
+
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:${port}/');
+});
+
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
