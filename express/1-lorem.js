502d32ed0223e7b82f22a37f1454998ec64617e7
Huanhua Su <karawakara@gmail.com>
2019-07-15 14:46:32 -0700

    Finish express Ex 1

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -1,5 +1,20 @@
 'use strict';
 
 var express = require('express'); // do not change this line
+var app = express();
+
+var port = Number(process.env.PORT);
+var hostname = 'localhost';
+
+app.get('/lorem', function (req, res) {
+
+    res.send('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+});
+
+app.listen(port, function () {
+
+    console.log(`Example app listening on port ${port}!`);
+
+});
 
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
