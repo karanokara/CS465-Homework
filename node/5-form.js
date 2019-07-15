8d9eca29130836ab7f7c215f94bc6836027b0f97
Huanhua Su <karawakara@gmail.com>
2019-07-15 11:32:53 -0700

    Finish node Ex 5

--- node/5-form.js
+++ node/5-form.js
@@ -2,6 +2,100 @@
 
 var http = require('http'); // do not change this line
 var querystring = require('querystring'); // do not change this line
+var url = require('url');
+
+
+var port = Number(process.env.PORT);
+var hostname = 'localhost';
+
+var msg = [];
+
+
+var handle = {
+    form: function (request, response, thisURL) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
+        response.write('<!DOCTYPE html>');
+        response.write('<html>');
+        response.write('<body>');
+        response.write('<form action="/new" method="post">');
+        response.write('<input type="text" name="name">');
+        response.write('<input type="text" name="message">');
+        response.write('<input type="submit" value="submit">');
+        response.write('</form>');
+        response.write('</body>');
+        response.write('</html>');
+
+        response.end();
+    },
+    new: function (request, response, thisURL) {
+
+        //console.log(' ---------------- request ----------------------:');
+        //console.log(request);
+
+        var data = '';
+        request.on('data', function (da) {
+            data = decodeURIComponent(da);
+        });
+        request.on('end', function () {
+            msg.push(querystring.parse(data));
+            console.log(msg);
+        });
+
+        response.statusCode = 200;
+        response.setHeader('content-type', 'text/plain');
+        response.write('thank you for your message');
+        response.end();
+
+    },
+    list: function (request, response, thisURL) {
+        response.statusCode = 200;
+        response.setHeader('content-type', 'text/plain');
+
+        for (var i = 0; i < msg.length; ++i) {
+            response.write(msg[i]['name'] + ': ' + msg[i]['message']);
+            if (i != (msg.length - 1))
+                response.write('\n');
+        }
+        response.end();
+    },
+    default: function (request, response) {
+        response.statusCode = 404;
+        response.setHeader('content-type', 'text/plain');
+        response.end();
+    }
+};
+
+http.createServer(function (request, response) {
+    var thisURL = url.parse(request.url, true);
+
+    // console.log('<request>:');
+    // console.log(request);
+    // console.log('<response>:');
+    // console.log(response);
+    // console.log(request.headers);
+
+    if (thisURL.pathname === '/favicon.ico') {
+        response.statusCode = 200;
+        response.setHeader('content-type', 'image/x-icon');
+        response.end();
+        return;
+    }
+
+    var call = handle[thisURL.pathname.substring(1)].bind(handle);
+    if (call) {
+        call(request, response, thisURL);
+    }
+    else {
+        handle['default'](request, response);
+    }
+
+}).listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:${port}/');
+});
+
 
 // http://localhost:8080/form should return the form as shown below
 //   res.writeHead(200, {
