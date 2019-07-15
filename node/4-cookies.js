e142a2a52cd498d063c668a89dc7363da8b6118d
Huanhua Su <karawakara@gmail.com>
2019-07-14 20:09:46 -0700

    Finish node Ex 4

--- node/4-cookies.js
+++ node/4-cookies.js
@@ -1,6 +1,54 @@
 'use strict';
 
 var http = require('http'); // do not change this line
+var url = require('url');
+
+
+var port = Number(process.env.PORT);
+var hostname = 'localhost';
+
+
+http.createServer(function (request, response) {
+    var thisURL = url.parse(request.url, true),
+        cookie = request.headers['cookie'];
+
+    //var requestH = request.
+    // console.log('<request>:');
+    // console.log(request);
+    // console.log('<response>:');
+    // console.log(response);
+    console.log(request.headers);
+
+    if (thisURL.pathname === '/favicon.ico') {
+        response.statusCode = 200;
+        response.setHeader('content-type', 'image/x-icon');
+        response.end();
+        return;
+    }
+
+    if (cookie) {
+        var cookies = cookie.split(';'),
+            lastPath = cookies[1].split('=')[1];
+
+        response.statusCode = 200;
+        response.setHeader('content-type', 'text/plain');
+        response.setHeader('set-cookie', ['hello=wor=ld', 'visited=' + thisURL.pathname]);
+        response.write('last time you visited "' + lastPath + '"');
+        response.end();
+    }
+    else {
+        response.statusCode = 200;
+        response.setHeader('content-type', 'text/plain');
+        response.setHeader('set-cookie', ['hello=wor=ld', 'visited=' + thisURL.pathname]);
+        response.write('you must be new');
+        response.end();
+    }
+
+
+}).listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:${port}/');
+});
+
 
 // note: handling for the requests should be generic and your server should appropriately respond to any path, including ones not listed below
 
