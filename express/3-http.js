25887322b9b94da8ceaf8dbb4167591de0a7c8a0
Huanhua Su <karawakara@gmail.com>
2019-07-16 21:56:42 -0700

    Doing express Ex 3

--- express/3-http.js
+++ express/3-http.js
@@ -1,7 +1,54 @@
 'use strict';
 
 var express = require('express'); // do not change this line
+var app = express();
 
+var port = Number(process.env.PORT) || 8080;
+var hostname = 'localhost';
+
+app.get('/attributes', function (request, response) {
+    console.log(request);
+    var qy = request.query;
+
+    var str = '<!DOCTYPE html>' +
+        '<html>' +
+        '<body>' +
+        '<table border="1">';
+    for (var key in qy) {
+        str += '<tr><td>' + key + '</td><td>' + qy[key] + '</td></tr>';
+    }
+    str += '</table></body></html>';
+    response.send(str);
+});
+
+
+app.get('/lorem', function (req, res) {
+
+    res.send('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+});
+
+app.use('/test', function (req, res) {
+    res.set({ 'content-type': 'text/plain' });
+    res.send('you have accessed "' + req.url.substring(1) + '" within test');
+});
+
+app.get('/', function (req, res) {
+    res.set({ 'content-type': 'text/plain' });
+    res.send('you have accessed the root');
+});
+
+// A route match any path
+// will be used as the final choice
+app.use(function (req, res, next) {
+    console.log('Last Fallback Called');
+    res.send('');
+});
+
+app.listen(port, hostname, function () {
+
+    console.log(`Example app listening on port ${port}!`);
+
+});
 
 
 // http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
