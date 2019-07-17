79d8b91103f0709c074a00fa0ac7dba00760b6d0
Huanhua Su <karawakara@gmail.com>
2019-07-16 22:56:51 -0700

    Finish express Ex 3

--- express/3-http.js
+++ express/3-http.js
@@ -6,40 +6,56 @@ var app = express();
 var port = Number(process.env.PORT) || 8080;
 var hostname = 'localhost';
 
-app.get('/attributes', function (request, response) {
-    console.log(request);
-    var qy = request.query;
+// http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
+app.get('/missing', function (req, res) {
+    res.status(404);
+    res.set('content-type', 'text/plain');
+    res.send('your princess is in another castle');
+});
 
-    var str = '<!DOCTYPE html>' +
-        '<html>' +
-        '<body>' +
-        '<table border="1">';
-    for (var key in qy) {
-        str += '<tr><td>' + key + '</td><td>' + qy[key] + '</td></tr>';
+// http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
+app.get('/redirect', function (req, res) {
+    var redirected = '/redirected';
+    res.set('content-type', 'text/plain');
+    res.redirect(redirected);
+});
+
+// http://localhost:8080/cache should return 'cache this resource' in plain text and set the cache max age to a day
+app.get('/cache', function (req, res) {
+    res.set({
+        'content-type': 'text/plain',
+        'cache-control': 'max-age=' + 24 * 60 * 60, // in second
+    });
+    res.send('cache this resource');
+});
+
+// http://localhost:8080/cookie should return 'i gave you a cookie' in plain text and set 'hello=world' as a cookie
+app.get('/cookie', function (req, res) {
+    res.set({
+        'content-type': 'text/plain',
+        'set-cookie': ['hello=world'],
+    });
+    res.send('i gave you a cookie');
+
+});
+
+// http://localhost:8080/check should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
+app.get('/check', function (req, res) {
+    var cookie = req.headers['cookie'];
+    res.set('content-type', 'text/plain');
+
+    if (cookie && cookie.includes('hello')) {
+        res.send('yes');
     }
-    str += '</table></body></html>';
-    response.send(str);
-});
+    else {
+        res.send('no');
+    }
 
-
-app.get('/lorem', function (req, res) {
-
-    res.send('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
-});
-
-app.use('/test', function (req, res) {
-    res.set({ 'content-type': 'text/plain' });
-    res.send('you have accessed "' + req.url.substring(1) + '" within test');
-});
-
-app.get('/', function (req, res) {
-    res.set({ 'content-type': 'text/plain' });
-    res.send('you have accessed the root');
 });
 
 // A route match any path
 // will be used as the final choice
-app.use(function (req, res, next) {
+app.use(function (req, res) {
     console.log('Last Fallback Called');
     res.send('');
 });
