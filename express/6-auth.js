78fdb97570c57b60219d8f10edfb4fd32106b496
Huanhua Su <karawakara@gmail.com>
2019-07-18 18:51:38 -0700

    Doing express Ex 6

--- express/6-auth.js
+++ express/6-auth.js
@@ -4,6 +4,37 @@ var express = require('express'); // do not change this line
 var passport = require('passport'); // do not change this line
 var strategy = require('passport-http'); // do not change this line
 
+var app = express();
+
+var port = Number(process.env.PORT) || 8080;
+var hostname = 'localhost';
+
+
+app.use(function (a, b, c, d) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+
+});
+
+
+// A route match any path
+// will be used as the final choice
+app.use(function (req, res) {
+
+    console.log('Last Fallback Called');
+
+    res.send('');
+});
+
+app.listen(port, hostname, function () {
+
+    console.log(`Example app listening on port ${port}!`);
+
+});
+
+
 // preface: use the passport middleware and only grant the user "test" with the password "logmein" access
 
 // note: should the server restart, the browser will still technically be logged in since we are using the http basic access authentication which lets the browser store and submit the username and the password at each request
